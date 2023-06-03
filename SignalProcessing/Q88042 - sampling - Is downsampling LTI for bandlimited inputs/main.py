4359cf7edc0eadf91f006e2c22d8d9da5970f58e
OverLordGoldDragon <john.muradeli@gmail.com>
2023-06-03 08:34:05 +0400

    Is downsampling LTI for bandlimited inputs? [UPD] - https://dsp.stackexchange.com/q/88042/50076

--- SignalProcessing/Q88042 - sampling - Is downsampling LTI for bandlimited inputs/main.py
+++ SignalProcessing/Q88042 - sampling - Is downsampling LTI for bandlimited inputs/main.py
@@ -2,7 +2,14 @@
 # https://dsp.stackexchange.com/q/88042/50076
 import numpy as np
 from numpy.fft import fft, ifft
-# from .toolkit import fft_upsample
+
+try:
+    from .toolkit import fft_upsample
+    got_fft_upsample = True
+except:
+    got_fft_upsample = False
+    import warnings
+    warnings.warn("`fft_upsample` not found! Some tests skipped.")
 
 # NOTE: linearity isn't tested!
 
@@ -20,13 +27,12 @@ def op(a, b, d, x_only):
     else:
         return ifft(fft(a[::d]) * fft(b[::d] * d))
 
-def div(a, b):
-    return int(np.ceil(a / b))
-
 #%%###########################################################################
 # Testing
 # -------
+# np.random.seed(0)  # uncomment for reproducible debugging
 optional_successes = {i: [] for i in range(5)}
+n_cases = 0
 
 for real in (True, False):
   for x_bandlimited in (True, False):
@@ -36,33 +42,34 @@ for real in (True, False):
             # duplicate case
             continue
         for N in (16, 40, 128):
-          for d in range(2, N//2, 2):
-            # d==N//2 is an edge case that can be handled but no need
+          for d in range(2, N//2 + 1, 2):
             for s in range(1, N):
               # prepare --------------------------------------------------------
               # check if loop is valid
-              if np.ceil(N / d) % 2 != 0 or not np.log2(d).is_integer():
+              if (N / d) % 2 != 0:
                   # `fft_upsample` doesn't handle this case, nor does it seem
                   # that many of such cases can be handled
                   continue
 
               # store reusables
-              both_bandlimited = bool(x_bandlimited and h_bandlimited)
-              one_bandlimited = bool(x_bandlimited or h_bandlimited)
-              not_bandlimited = bool(not x_bandlimited and not h_bandlimited)
+              both_bandlimited = (x_bandlimited and h_bandlimited)
+              one_bandlimited  = (x_bandlimited or  h_bandlimited)
+              not_bandlimited  = (not x_bandlimited and not h_bandlimited)
+              bandlimited = ((x_only and x_bandlimited) or
+                             (not x_only and both_bandlimited))
 
               # generate filter, signal
               x, h = crandn(N), crandn(N)
               if real:
                   x, h = x.real, h.real
+              xf, hf = fft(x), fft(h)
+              zero_idxs = (slice(N//d//2, -(N//d//2 - 1)) if N//d != 2 else
+                           slice(1, None))
               if x_bandlimited:
-                  xf = fft(x)
-                  xf[N//d//2:-(N//d//2 - 1)] = 0
-                  x = ifft(xf)
+                  xf[zero_idxs] = 0
               if h_bandlimited:
-                  hf = fft(h)
-                  hf[N//d//2:-(N//d//2 - 1)] = 0
-                  h = ifft(hf)
+                  hf[zero_idxs] = 0
+              x, h = ifft(xf), ifft(hf)
               if real:
                   assert np.allclose(max(abs(x.imag)), 0)
                   assert np.allclose(max(abs(h.imag)), 0)
@@ -76,8 +83,9 @@ for real in (True, False):
               o1b = op(np.roll(x, s), h, d, x_only)
 
               # get upsampled outputs
-              o1au, o1bu = [fft_upsample(o, d, time_to_time=True)
-                            for o in (o1a, o1b)]
+              if got_fft_upsample:
+                  o1au, o1bu = [fft_upsample(o, d, time_to_time=True)
+                                for o in (o1a, o1b)]
 
               # validate -------------------------------------------------------
               # prepare info in case assert fails
@@ -93,16 +101,15 @@ for real in (True, False):
                   0: True,
                   1: False,
                   2: (s / d).is_integer(),
-                  3: ((x_only and (x_bandlimited or (s / d).is_integer())) or
-                      (one_bandlimited and (s / d).is_integer())),
-                  4: both_bandlimited,
+                  3: (s / d).is_integer() or bandlimited,
+                  4: bandlimited,
               }
               optional = {
                   0: False,
                   1: True,
                   # only integer `s/d` must pass
                   2: False if cond[2] else True,
-                  # only bandlimited or integer `s/d` must pass
+                  # only integer `s/d` or bandlimited must pass
                   3: False if cond[3] else True,
                   # only bandlimited must be roLTI
                   4: False if cond[4] else True,
@@ -114,16 +121,18 @@ for real in (True, False):
               # shifted input <=> shifted output
               results[0] = (cond[0] == np.allclose(o0b, np.roll(o0a, s)))
               # [LTI]
-              # shift of subsampling <=> subsampling of shift
-              results[1] = (cond[1] == np.allclose(np.roll(o1a, div(s, d)), o1b))
+              # subsampling of shift <=> shift of subsampling
+              results[1] = (cond[1] == np.allclose(o1b, np.roll(o1a, s//d)))
               # [fLTI]
-              # shift of subsampling <=> subsampling of shift; integer `s/d`
-              results[2] = (cond[2] == np.allclose(np.roll(o1a, div(s, d)), o1b))
+              # subsampling of shift <=> shift of subsampling; integer `s/d`
+              results[2] = (cond[2] == np.allclose(o1b, np.roll(o1a, s//d)))
               # [rLTI]
               # upsampled subsampling of shift <=> shift of upsampled subsampling
-              results[3] = (cond[3] == np.allclose(np.roll(o1au, s), o1bu))
+              if got_fft_upsample:
+                  results[3] = (cond[3] == np.allclose(o1bu, np.roll(o1au, s)))
               # [roLTI] upsampled subsampling of shift <=> shift
-              results[4] = (cond[4] == np.allclose(o1bu, o0b))
+              if got_fft_upsample:
+                  results[4] = (cond[4] == np.allclose(o1bu, o0b))
 
               # run assertions on non-optionals, append info about the rest
               for i in results:
@@ -137,12 +146,16 @@ for real in (True, False):
                       assert results[0], (
                           "0:\n" "the operation, unrelated to subsampling, "
                           "isn't LTI")
+              n_cases += 1
 
 #%%###########################################################################
 # Trivia
 # ------
 print("Number of optional (unexpected) successes:\n  " +
-      "\n  ".join(f"Case {i}: {len(v)}" for i, v in optional_successes.items()))
+      "\n  ".join("Case {}: {:>4} ({:>4.3g}%)".format(
+          i, len(v), 100*len(v)/n_cases)
+                  for i, v in optional_successes.items()))
+
 print("\nExample optional successes for case 2:\n  " +
       ", ".join(list(cfg)) + "\n  " +
       "\n  ".join(str(v) for v in optional_successes[2][:5]))
